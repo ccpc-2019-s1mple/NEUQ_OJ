09a8ad9b3d3a675613fcf306488d9e3966960f7c
PlanC <2386116982@qq.com>
2019-04-23 20:08:15 +0800

    update repo

---
+++ C/1019.c
@@ -0,0 +1,25 @@
+//1019 Incorrect
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+	int n;
+	int i;
+	
+	while (1) {
+		scanf("%d", &n);
+		if (n == -1) break;
+		if (n == 1) {
+			printf("55\n");
+			continue;
+		}
+		printf("5");
+		for (i = 1; i < n; i++) printf("0");
+		printf("5");
+		for (i = 1; i < n; i++) printf("0");
+		printf("\n");
+	}
+	
+	return 0;
+}
