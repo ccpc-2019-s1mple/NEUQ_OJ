cb26ae58f41f1f23771c0d5cacdc2657cbc82980
PlanC <planc2333@outlook.com>
2019-04-25 10:32:34 +0800

    Update 1018.c
    /tmp/ccF73jV6.o: In function `main': main.c:(.text.startup+0xd7): undefined reference to `strrev' main.c:(.text.startup+0xe1): undefined reference to `strrev' collect2: error: ld returned 1 exit status

--- C/1018.c
+++ C/1018.c
@@ -1,18 +1,42 @@
-//1018 Incorrect
+//1018
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	int n;
+	int n = 0;
 	int i = 0;
-	long long int A;
-	long long int B;
+	int j = 0;
+	int carry = 0;
+	int longer = 0;
+	char bignumber1[1001] = {'0'}, bignumber2[1001] = {'0'};
+	char bigresult[1001] = {'0'};
+	
 	scanf("%d", &n);
 	while (n--) {
-		i++;
-		scanf("%lld %lld", &A, &B);
-		printf("case %d:\n%lld + %lld = %lld\n",i , A, B, A + B);
+		scanf("%s", bignumber1);
+		scanf("%s", bignumber2);
+		strrev(bignumber1);
+		strrev(bignumber2);
+		longer = strlen(bignumber1) > strlen(bignumber2) ? strlen(bignumber1) : strlen(bignumber2);
+		for (i = 0; i < longer; i++) {
+			if (((int)bignumber1[i] - 48) + ((int)bignumber2[i] - 48) + carry > 9) {
+				bigresult[i] = (((int)bignumber1[i] - 48) + ((int)bignumber2[i] - 48) + carry) / 10;
+				carry = (((int)bignumber1[i] - 48) + ((int)bignumber2[i] - 48) + carry) % 10;
+			}
+			else {
+				bigresult[i] = ((int)bignumber1[i] - 48) + ((int)bignumber2[i] - 48) + carry;
+				carry = 0;
+			}
+		}
+		j++;
+		printf("Case %d:\n", j);
+		for (i = 0; i < strlen(bigresult); i++) {
+			printf("%d", (int)bigresult[i]);
+		}
+		printf("\n");
 	}
+	//printf("%s %d", bignumber1, strlen(bignumber1));
 	
 	return 0;
-} 
+}
