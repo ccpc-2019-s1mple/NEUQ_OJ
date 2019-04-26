a9903f755f4a82824cdcc7f9641ae2ddfcb2e796
PlanC <planc2333@outlook.com>
2019-04-26 16:11:13 +0800

    Update 1033.c

--- C/1033.c
+++ C/1033.c
@@ -7,5 +7,5 @@ int main() {
 	scanf("%d %d %d", &array[0][0], &array[0][1], &array[0][2]);
 	scanf("%d %d %d", &array[1][0], &array[1][1], &array[1][2]);
 	scanf("%d %d %d", &array[2][0], &array[2][1], &array[2][2]);
-	printf("%d", array[0][0] + array[1][1] + array[2][2])
+	printf("%d", array[0][0] + array[1][1] + array[2][2]);
 }
