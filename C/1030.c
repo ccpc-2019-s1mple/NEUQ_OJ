541698a1e5a65e35b8253895ec13b0d1510b0633
PlanC <planc2333@outlook.com>
2019-04-28 09:00:14 +0800

    Update 1030.c

--- C/1030.c
+++ C/1030.c
@@ -1,13 +1,15 @@
 //1030
 
-#include <math.h> 
 #include <stdio.h>
 
 int main() {
 	int R = 0;
+	float area = 0.0;
+	
 	for (R = 1; R <= 10; R++) {
-		if (R * R * M_PI >= 40.0 && R * R * M_PI <= 90.0) {
-			printf("r=%d area=%.2f\n", R, R * R * M_PI);
-		}
+		area = R * R * 3.1415926535;
+		if (area >= 40.0 && area <= 90.0) printf("r=%d area=%2.2f\n", R, area);
 	}
+	
+	return 0;
 }
