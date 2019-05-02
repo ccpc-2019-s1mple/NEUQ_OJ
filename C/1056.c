aa5b2ef39eb60a584f08beb0200ea4e9b94875df
PlanC <2386116982@qq.com>
2019-05-02 12:18:07 +0800

    update repo

--- C/1056.c
+++ C/1056.c
@@ -3,25 +3,23 @@
 #include <stdio.h>
 
 int main() {
-	int n = 0;
 	int i = 0;
 	int j = 0;
-	int pointer = 0;
+	int N = 0;
+	int array[1000] = {0};
 	
-	scanf("%d", &n);
-	int List[10000] = {0};
-	for (i = 2; i <= n; i++) List[i] = i;
+	scanf("%d", &N);
 	
-	for (i = 2; i <= n; i++) {
-		for (j = i; j <= n; j++) {
-			if (j % i == 0) {
-				List[j] = 0;
-			}
+	for (i = 2; i <= N; i++) array[i] = i;
+	
+	for (i = 2; i <= N; i++) {
+		for (j = i + 1; j <= N; j++) {
+			if (j % i == 0) array[j] = 0;
 		}
 	}
 	
-	for (i = 2; i < n; i++) {
-		if (List[i] != 0) printf("%d\n", i);
+	for (i = 2; i <= N; i++) {
+		if (array[i] != 0) printf("%d\n", array[i]);
 	}
 	
 	return 0;
