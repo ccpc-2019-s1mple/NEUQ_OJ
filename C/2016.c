4949b6a24b95eccfc95a85634957cca544c44959
PlanC <2386116982@qq.com>
2019-05-05 13:31:56 +0800

    update repo

--- C/2016.c
+++ C/2016.c
@@ -3,16 +3,13 @@
 #include <stdio.h>
 
 int main() {
-	long long i = 0;
 	long long l = 0,r = 0;
 	scanf("%lld %lld", &l, &r);
 	
-	for (i = l; i <= r; i++) {
-		if ((i | i + 1) > r) {
-			printf("%lld", i);
-			break;
-		}
-	}
+	while((l | (l + 1)) <= r){
+        l |= (l + 1);
+    }
+    printf("%d", l);
 	
 	return 0;
 }
---
+++ C/2155.c
@@ -0,0 +1,15 @@
+//2155
+
+#include <stdio.h>
+
+int main() {
+	int T = 0;
+	int length = 0;
+	
+	scanf("%d", &t);
+	
+	while (T--) {
+		scanf("%d", &length);
+		
+	}
+}
