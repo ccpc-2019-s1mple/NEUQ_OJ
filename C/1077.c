5a9f82051bbc8bca36e1149d62ce7abcaa10c2c3
PlanC <planc2333@outlook.com>
2019-05-09 09:15:18 +0800

    update repo

--- C/1077.c
+++ C/1077.c
@@ -3,7 +3,24 @@
 #include <stdio.h>
 
 int main() {
+	int i = 0;
+	int j = 0;
+	int temp = 0;
+	int array[3] = {0};
 	
+	for (i = 0; i < 3; i++) scanf("%d", &array[i]);
+	
+	for (i = 0; i < 3 - 1; i++) {
+		for (j = 0; j < 3 - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+	
+	for (i = 0; i < 3; i++) printf("%d ", array[i]);
 	
 	return 0;
 }
